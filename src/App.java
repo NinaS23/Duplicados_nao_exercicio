eb28b00dfaff29443614888470c3265cb23d4753
MariaDuda <mariabevenuto062@gmail.com>
2023-01-16 16:47:37 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        List<String> pokemons = new ArrayList<>();
+
+        pokemons.add("Ditto");
+        pokemons.add("Snorlax");
+        pokemons.add("Pichu");
+        pokemons.add("Charizard");
+        pokemons.add("Lucario");
+        pokemons.add("Meowth");
+
+        List<String> pokemonsDoAsh = new ArrayList<>();
+
+        pokemonsDoAsh.add("Lucario");
+        pokemonsDoAsh.add("Snorlax");
+        pokemonsDoAsh.add("Charizard");
+        pokemonsDoAsh.add("Krookodile");
+        pokemonsDoAsh.add("Gengar");
+        
+        List<String> pokemonsIguais = new ArrayList<>();
+
+        for(int i = 0; i < pokemons.size(); i++){
+           if(pokemonsDoAsh.contains(pokemons.get(i))){
+              pokemonsIguais.add(pokemons.get(i));
+           }
+        }
+        
+        System.out.println(pokemonsIguais);
+
+    }
+
+}
