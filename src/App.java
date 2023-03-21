153dacc539c01907656c309b71282fabf4db5353
DUDA <mariabevenuto062@gmail.com>
2023-03-21 14:32:32 -0300

    rm: throws Exception

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         List<String> pokemons = new ArrayList<>();
 
         pokemons.add("Ditto");
